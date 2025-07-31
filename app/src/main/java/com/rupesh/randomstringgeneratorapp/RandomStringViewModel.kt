e42697a854277ca05b274f512eafa0157dbabb78
Owner <rupeshkharjul27@gmail.com>
2025-07-31 10:05:18 +0530

    added couroutine to fetch data from content provider

--- app/src/main/java/com/rupesh/randomstringgeneratorapp/RandomStringViewModel.kt
+++ app/src/main/java/com/rupesh/randomstringgeneratorapp/RandomStringViewModel.kt
@@ -5,36 +5,67 @@ import android.database.Cursor
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.json.JSONObject
 
 class RandomStringViewModel : ViewModel() {
     var stringDataState = mutableStateListOf<StringData>()
 
     fun generateString(contentResolver: ContentResolver, length: Int) {
-        val uri = Uri.parse(ContentContract.CONTENT_URI)
-        val cursor: Cursor? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            val bundle = Bundle().apply {
-                putInt(ContentResolver.QUERY_ARG_LIMIT, length)
-            }
-            contentResolver.query(uri, null, bundle, null)
-        } else {
-            contentResolver.query(uri, null, null, null, null)
-        }
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val uri = Uri.parse(ContentContract.CONTENT_URI)
+                val cursor: Cursor? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                    val bundle = Bundle().apply {
+                        putInt(ContentResolver.QUERY_ARG_LIMIT, length)
+                    }
+                    contentResolver.query(uri, null, bundle, null)
+                } else {
+                    contentResolver.query(uri, null, null, null, null)
+                }
 
+                cursor?.use {
+                    if (it.moveToFirst()) {
+                        try {
+                            val jsonStr =
+                                it.getString(it.getColumnIndexOrThrow(ContentContract.JsonEntry.DATA_OBJ))
+                            val json =
+                                JSONObject(jsonStr).getJSONObject(ContentContract.JsonEntry.RANDOM_TEXT_JSON_OBJ)
+                            val value = json.getString(ContentContract.JsonEntry.VALUE)
+                            val len = json.getInt(ContentContract.JsonEntry.LENGTH)
+                            val created = json.getString(ContentContract.JsonEntry.CREATED)
 
-        cursor?.use {
-            if (it.moveToFirst()) {
-                val jsonStr = it.getString(it.getColumnIndexOrThrow(ContentContract.JsonEntry.DATA_OBJ))
-                val json = JSONObject(jsonStr).getJSONObject(ContentContract.JsonEntry.RANDOM_TEXT_JSON_OBJ)
-                val value = json.getString(ContentContract.JsonEntry.VALUE)
-                val len = json.getInt(ContentContract.JsonEntry.LENGTH)
-                val created = json.getString(ContentContract.JsonEntry.CREATED)
-                stringDataState.add(0, StringData(value = value, length = len, created = created))
+                            // Post to main thread safely
+                            withContext(Dispatchers.Main) {
+                                stringDataState.add(
+                                    0,
+                                    StringData(value = value, length = len, created = created)
+                                )
+                            }
+
+                        } catch (e: Exception) {
+                            Log.d("generateString", "Error while json parsing ${e.message}")
+                        }
+                    }
+                }
+            } catch (e : Exception){
+                Log.d("generateString", "Error when querying content provider ${e.message}")
             }
         }
     }
+
 
     fun clearAll() {
         stringDataState.clear()
