8db6d26b6ca019f667a2a12895e1a3b3366a4d6d
Owner <rupeshkharjul27@gmail.com>
2025-07-31 18:37:17 +0530

    added gson to parse the json

--- app/src/main/java/com/rupesh/randomstringgeneratorapp/RandomStringViewModel.kt
+++ app/src/main/java/com/rupesh/randomstringgeneratorapp/RandomStringViewModel.kt
@@ -6,28 +6,24 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.json.JSONObject
 
 class RandomStringViewModel : ViewModel() {
-    var stringDataState = mutableStateListOf<StringData>()
+    var stringDataState = mutableStateListOf<StringData>() /*Use mutable state to update the data as list*/
 
     fun generateString(contentResolver: ContentResolver, length: Int) {
+        
+        /* Use Coroutine scope to get data from ContentResolver */
         viewModelScope.launch(Dispatchers.IO) {
             try {
                 val uri = Uri.parse(ContentContract.CONTENT_URI)
-                val cursor: Cursor? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                val cursor: Cursor? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) { /*In older version we can not pass bundle*/
                     val bundle = Bundle().apply {
                         putInt(ContentResolver.QUERY_ARG_LIMIT, length)
                     }
@@ -38,20 +34,19 @@ class RandomStringViewModel : ViewModel() {
 
                 cursor?.use {
                     if (it.moveToFirst()) {
-                        try {
-                            val jsonStr =
-                                it.getString(it.getColumnIndexOrThrow(ContentContract.JsonEntry.DATA_OBJ))
-                            val json =
-                                JSONObject(jsonStr).getJSONObject(ContentContract.JsonEntry.RANDOM_TEXT_JSON_OBJ)
-                            val value = json.getString(ContentContract.JsonEntry.VALUE)
-                            val len = json.getInt(ContentContract.JsonEntry.LENGTH)
-                            val created = json.getString(ContentContract.JsonEntry.CREATED)
 
-                            // Post to main thread safely
+                        val jsonStr = it.getString(it.getColumnIndexOrThrow(ContentContract.JsonEntry.DATA_OBJ))
+                        try {
+
+                            val payload = Gson().fromJson(jsonStr, RandomTextPayload::class.java)  // Use Gson to parse the json
+                            val randomText = payload.randomText
+
+
+                            // Switch to main thread to update data on activity
                             withContext(Dispatchers.Main) {
                                 stringDataState.add(
                                     0,
-                                    StringData(value = value, length = len, created = created)
+                                    StringData(value = randomText.value, length = randomText.length, created = randomText.created)
                                 )
                             }
 
--- app/src/main/java/com/rupesh/randomstringgeneratorapp/StringData.kt
+++ app/src/main/java/com/rupesh/randomstringgeneratorapp/StringData.kt
@@ -2,6 +2,10 @@ package com.rupesh.randomstringgeneratorapp
 
 import java.util.UUID
 
+data class RandomTextPayload(
+    val randomText: StringData
+)
+
 data class StringData(
     val id: UUID = UUID.randomUUID(),
     val value: String,
